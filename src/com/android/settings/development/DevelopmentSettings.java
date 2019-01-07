c2f9f8155cb3eae99379d843be880a20d0881811
Shilin Victor <chrono.monochrome@gmail.com>
2019-01-08 01:12:19 +0300

    Settings: Introduce Substratum changes.
    * Add toggle to force bypass of authorisation checks in the
    SubstratumService. This is only accessible to owner accounts.
    * Add ApplicationsState filter for Substratum overlays in All
    apps list.
    
    Change-Id: I36b88aedd63935bbf71f480beaab7e3b77556a60

--- src/com/android/settings/development/DevelopmentSettings.java
+++ src/com/android/settings/development/DevelopmentSettings.java
@@ -262,6 +262,8 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
 
     private static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
 
+    private static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
+
     private static final int RESULT_DEBUG_APP = 1000;
     private static final int RESULT_MOCK_LOCATION_APP = 1001;
 
@@ -287,6 +289,9 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     private boolean mDontPokeProperties;
     private EnableAdbPreferenceController mEnableAdbController;
     private SwitchPreference mAdbOverNetwork;
+
+    private SwitchPreference mForceAuthorizeSubstratumPackages;
+
     private Preference mClearAdbKeys;
     private SwitchPreference mEnableTerminal;
     private RestrictedSwitchPreference mKeepScreenOn;
@@ -496,10 +501,13 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         mAllPrefs.add(mMSOB);
         mMSOB.setOnPreferenceChangeListener(this);
 
+        mForceAuthorizeSubstratumPackages = findAndInitSwitchPref(FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES);
+
         if (!mUm.isAdminUser()) {
             disableForUser(mClearAdbKeys);
             disableForUser(mEnableTerminal);
             disableForUser(mPassword);
+            disableForUser(mForceAuthorizeSubstratumPackages);
         }
 
         mDebugAppPref = findPreference(DEBUG_APP_KEY);
@@ -946,6 +954,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         updateBluetoothA2dpConfigurationValues();
         updateAdbOverNetwork();
         updateRootAccessOptions();
+        updateForceAuthorizeSubstratumPackagesOptions();
     }
 
     private void resetMSOBOptions() {
@@ -993,6 +1002,17 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         } else {
             mAdbOverNetwork.setSummary(R.string.adb_over_network_summary);
         }
+    }
+
+    private void writeForceAuthorizeSubstratumPackagesOptions() {
+        Settings.Secure.putInt(getActivity().getContentResolver(),
+                Settings.Secure.FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES,
+                mForceAuthorizeSubstratumPackages.isChecked() ? 1 : 0);
+    }
+
+    private void updateForceAuthorizeSubstratumPackagesOptions() {
+        mForceAuthorizeSubstratumPackages.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(),
+                Settings.Secure.FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES, 0) != 0);
     }
 
     private void resetDangerousOptions() {
@@ -2823,6 +2843,8 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
             writeForceResizableOptions();
         } else if (preference == mBluetoothShowDevicesWithoutNames) {
             writeBluetoothShowDevicesWithoutUserFriendlyNameOptions();
+        } else if (preference == mForceAuthorizeSubstratumPackages) {
+            writeForceAuthorizeSubstratumPackagesOptions();
         } else if (preference == mBluetoothDisableAbsVolume) {
             writeBluetoothDisableAbsVolumeOptions();
         } else if (preference == mBluetoothEnableInbandRinging) {
